1b9c3b5764045a51b37eb44b65506023c73f5f29
jongsungpark5103 <jongsung5103@gmail.com>
2023-10-19 17:50:05 +0900

    오류수정

--- src/main/java/com/springdatajpa/practice/drink/repository/TypeRepository.java
+++ src/main/java/com/springdatajpa/practice/drink/repository/TypeRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @EnableJpaRepositories
 public interface TypeRepository extends JpaRepository<Type, Integer> {
-    @Query(value = "SELECT drink_type_code, drink_type_name FROM tbl_type ORDER BY drink_type_code ASC", nativeQuery = true)
+    @Query(value = "SELECT type_code, type_name FROM tbl_type ORDER BY type_code", nativeQuery = true)
     public List<Type> findAllType();
 }
